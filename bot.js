38aee8d6861ab9f9e8803df39f20ce3c9e6a619b
CubeRuben <egor.petrorov.03@mail.ru>
2020-02-10 22:57:07 +0300

    Added loging bad words

--- bot.js
+++ bot.js
@@ -1,11 +1,13 @@
 const Discord = require("discord.js");
 const bot = new Discord.Client();
+const fs = require('fs');
 const login = require('./login.json');
 const translate = require('translate-google');
 
 var prefix = ".";
 
 var answersOnPing = [ "Yes?", "No", "Maybe", "No you", "Maybe we will drink the VODKA", "Yes", "Yes you may", "Stay me alone", "I want to die" ];
+var badWords = [ ];
 
 function sendWedhook(message, channel, username, avatar) 
 {
@@ -18,12 +20,14 @@ function sendWedhook(message, channel, username, avatar)
 
 bot.on('ready', () => {
     console.log("Bot loaded");
+
+    badWords = fs.readFileSync("bad_words.txt").toString().toUpperCase().split(' ');
 });
 
 bot.on('message', (message) => {
-    let args = message.content.replace('   ', ' ').replace('  ', ' ').split(' ');
+    let args = message.content.toUpperCase().replace('   ', ' ').replace('  ', ' ').split(' ');
     
-    if (message.member != null) 
+    if ((message.member != null) && (message.author.id != bot.user.id)) 
     {
         if (message.content.replace('!', '').includes(message.guild.members.get(bot.user.id).toString())) 
         {
@@ -31,11 +35,68 @@ bot.on('message', (message) => {
             return;
         }
 
+        for (let i = 0; i < badWords.length; i++) 
+        {
+            if (message.content.toUpperCase().includes(badWords[i])) 
+            {
+                message.guild.channels.get('676438061135167519').send("**User** " + message.member + " **use bad word** " + badWords[i] + " **in channel** " + message.channel + " **in message** " + message.url);
+                break;
+            }
+        }
+
         //Обработчик команд
-        switch (args[0].toUpperCase()) 
+        switch (args[0]) 
         {
             case prefix + "TRANSLATE":
                 translate(message.content.substring(args[0].length + args[1].length + 2, message.content.length), { to: args[1] }).then(res => sendWedhook(res, message.channel, message.author.username, message.author.avatarURL)).catch(err => message.channel.send(err));
+            break;
+            case prefix + "ADDBADWORD":
+                if (message.member.hasPermission("READ_MESSAGE_HISTORY")) 
+                {
+                    if (args[1]) 
+                    { 
+                        let string = fs.readFileSync("bad_words.txt").toString();
+                        
+                        if (!string.includes(args[1])) 
+                        { 
+                            fs.writeFileSync("bad_words.txt", string + " " + args[1]);  
+                            badWords.push(args[1]);
+                            message.channel.send("**Successfully added**");
+                        } 
+                        else 
+                        {
+                            message.channel.send("**Already added**");
+                        }
+                    } 
+                    else 
+                    {
+                        message.channel.send("**Missing bad word**");
+                    }
+                }
+            break;
+            case prefix + "REMOVEBADWORD":
+                if (message.member.hasPermission("READ_MESSAGE_HISTORY")) 
+                {
+                    if (args[1]) 
+                    {
+                        let string = fs.readFileSync("bad_words.txt").toString();
+
+                        if (string.includes(args[1])) 
+                        {
+                            fs.writeFileSync("bad_words.txt", string.replace(args[1], '').replace('  ', ' '));
+                            badWords.splice(badWords.indexOf(args[1]));
+                            message.channel.send("**Successfully removed**");
+                        } 
+                        else 
+                        {
+                            message.channel.send("**Bad words list is not including this word**");
+                        }
+                    } 
+                    else 
+                    {
+                        message.channel.send("**Missing bad word**");
+                    }
+                }
             break;
         }
     }
